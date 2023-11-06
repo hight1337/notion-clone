e903e18549ee6e4d094b8a5e6da915f61e93afc6
vladislavCh <hight1337@gmail.com>
2023-11-06 19:42:31 +0200

    fix: `publish` banner

--- app/(main)/_components/Navbar.tsx
+++ app/(main)/_components/Navbar.tsx
@@ -2,6 +2,8 @@
 
 import { useQuery } from 'convex/react';
 import { useParams } from 'next/navigation';
+import { useMediaQuery } from 'usehooks-ts';
+
 import { MenuIcon } from 'lucide-react';
 
 import { api } from '@/convex/_generated/api';
@@ -12,8 +14,6 @@ import { Banner } from './Banner';
 import { Menu } from './Menu';
 import { Publish } from './Publish';
 
-import { cn } from '@/lib/utils';
-
 interface NavbarProps {
   isCollapsed: boolean;
   onResetWidth: () => void;
@@ -21,6 +21,7 @@ interface NavbarProps {
 
 export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
   const params = useParams();
+  const isMobile = useMediaQuery('(max-width: 768px)');
 
   const document = useQuery(api.documents.getById, {
     documentId: params.documentId as Id<'documents'>,
@@ -28,12 +29,7 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
 
   if (document === undefined) {
     return (
-      <nav
-        className={cn(
-          'bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between',
-          !isCollapsed && 'opacity-0',
-        )}
-      >
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between">
         <Title.Skeleton />
         <div className="flex items-center gap-x-2">
           <Menu.Skeleton />
@@ -46,30 +42,28 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
     return null;
   }
 
+  const mobile = isMobile && isCollapsed;
+  const desktop = !isMobile;
+
   return (
     <>
-      <nav
-        className={cn(
-          'bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center gap-x-4',
-          !isCollapsed && 'opacity-0',
-        )}
-      >
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center gap-x-4">
         {isCollapsed && (
-          <>
-            <MenuIcon
-              role="button"
-              onClick={onResetWidth}
-              className="h-6 w-6 text-muted-foreground"
-            />
-            <div className="flex items-center justify-between w-full">
-              <Title initialData={document} />
-              <div className="flex items-center gap-x-2">
-                <Publish initialData={document} />
-                <Menu documentId={document._id} />
-              </div>
-            </div>
-          </>
+          <MenuIcon
+            role="button"
+            onClick={onResetWidth}
+            className="h-6 w-6 text-muted-foreground"
+          />
         )}
+        {mobile || desktop ? (
+          <div className="flex items-center justify-between w-full">
+            <Title initialData={document} />
+            <div className="flex items-center gap-x-2">
+              <Publish initialData={document} />
+              <Menu documentId={document._id} />
+            </div>
+          </div>
+        ) : null}
       </nav>
       {document.isArchived && <Banner documentId={document._id} />}
     </>
